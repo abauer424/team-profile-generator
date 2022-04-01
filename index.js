af3d827ab184923bb84d6e95afd283138cd5a030
Amber Bauer <amberbauer@Ambers-MacBook-Pro.local>
2022-04-01 13:37:24 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
 //Included packages needed for this application
 const inquirer = require('inquirer');
-const jest  = require('jest');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const path = require('path');
 
 
-//user adds input
-//generates an html file
-
 // Created an array of questions for user input
 const questions = [
 
-    //1. Project Title
-        {
-            type: 'input',
-            name: 'projectTitle',
-            message: 'What is your project title?'
-          },
-    //2. Description
+    //Add team member
     {
         type: 'input',
-        name: 'description',
-        message: 'Provide a short description explaining the what, why, and how of your project.'
-    },    
-    
-    ];
-    //Created a function to write README file
-    function writeToFile(fileName, data) {
-        return fs.writeFileSync(path.join(__dirname,'./dist/'+ fileName),data)
-    }
-    
-    // TODO: Create a function to initialize app
-    function init() {
-        inquirer.prompt(questions).then(answers => {
-            console.log(answers);
-            writeToFile('index.html', generateMarkdown(answers));
-        
-        }) 
-    }
-    
-    // Function call to initialize app
-    init();
-    
+        name: 'getRole',
+        message: 'Which type of team member would you like to add?',
+    },
+    //1. Engineer Name
+    {
+        type: 'input',
+        name: 'getName',
+        message: 'What is your engineers name?'
+    },
+    //2. Engineer ID
+    {
+        type: 'input',
+        name: 'getId',
+        message: 'What is your engineers ID?'
+    },
+
+    //3. Engineer Email
+    {
+        type: 'input',
+        name: 'getEmail',
+        message: 'What is your engineers email?'
+    },
+
+    //4. Engineer GitHub username
+    {
+        type: 'input',
+        name: 'getGithub',
+        message: 'What is your engineers github?'
+    },
+ 
+];
+
+//Created a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(__dirname, './dist/' + fileName), data)
+}
+
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        console.log(answers);
+        writeToFile('index.html', generateMarkdown(answers));
+
+    })
+}
+
+// Function call to initialize app
+init();
+
