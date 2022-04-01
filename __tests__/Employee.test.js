943d8f460829c063eeef3ffb42feb3da69b9b648
AJB <designbyamber1@gmail.com>
2022-03-31 20:57:16 -0400

    start employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,16 @@
 //name, id, email, getname(), getId(), getEmail(), getRole(),
+const Employee = require('../lib/Employee');
+
+test('creates employee onject', () => {
+    const employee = new Employee('Amber', 1, 'amber@yahoo.com');
+    expect(employee.name).toBe('Amber');
+    expect(employee.id).toBe(1);
+    expect(employee.email).toBe('amber@yahoo.com');
+    expect(employee.role).toBe('Employee');
+
+})
+
+test('get name of employee as object', () => {
+    const employee = new Employee('Amber', 1, 'amber@yahoo.com');
+    expect(employee.getName()).toHaveProperty('name');
+})
