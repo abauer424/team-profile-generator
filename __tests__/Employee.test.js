c26304eaf67bfe64498d264b2527576f955a0712
AJB <designbyamber1@gmail.com>
2022-03-31 21:26:46 -0400

    started testing

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 //name, id, email, getname(), getId(), getEmail(), getRole(),
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee.js');
 
-test('creates employee onject', () => {
+test('creates employee object', () => {
     const employee = new Employee('Amber', 1, 'amber@yahoo.com');
     expect(employee.name).toBe('Amber');
     expect(employee.id).toBe(1);
@@ -14,3 +14,22 @@ test('get name of employee as object', () => {
     const employee = new Employee('Amber', 1, 'amber@yahoo.com');
     expect(employee.getName()).toHaveProperty('name');
 })
+
+test('get id as object', () => {
+    const employee = new Employee('Amber', 1, 'amber@yahoo.com');
+    expect(employee.getId()).toHaveProperty(1);
+})
+
+test('get email as object', () => {
+    const employee = new Employee('Amber', 1, 'amber@yahoo.com');
+    expect(employee.getEmail()).toHaveProperty('amber@yahoo.com');
+})
+
+test('get role as object', () => {
+    const employee = new Employee('Amber', 1, 'amber@yahoo.com');
+    expect(employee.getRole()).toHaveProperty('role');
+})
+
+
+
+
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,37 @@
 //name, id, email, getname(), getId(), getEmail(), getRole(), officeNumber
+const Manager=require("../lib/Manager.js")
+​
+describe("Test Manager",()=>{
+    describe("test all properties of manager",()=>{
+         describe(" test email",()=>{
+              const manager= new Manager("Phil",1,"phil@yahoo.com","232341")
+               
+          
+              expect(manager.email).toEqual(expect.any(String))
+              expect(manager.email).toEqual("phil@yahoo.com")
+         })
+​
+         describe(" test id",()=>{
+             
+        })
+        describe(" test name",()=>{
+             
+        })
+​
+        describe(" test office",()=>{
+             
+        })
+    })
+​
+    describe("test all the methods of manager",()=>{
+         describe("getname",()=>{
+             const manager=new Manager("Chris",2,"chris@yahoo.com","234234")
+             expect(manager.getName()).toEqual("Chris")
+         })
+​
+         describe("getID",()=>{
+            const manager=new Manager("Chris",2,"chris@yahoo.com","234234")
+            expect(manager.getId()).toEqual(2)
+        })
+    })
+})
